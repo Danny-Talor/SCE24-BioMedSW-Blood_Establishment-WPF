c9303f9e3e69ad6a92439033e3bb8bf1464b186c
Danny Tal'or <dannytalor96@gmail.com>
2024-06-18 20:37:08 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SCE24_BioMedSW_Blood_Establishment_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
