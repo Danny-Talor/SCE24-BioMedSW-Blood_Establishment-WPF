d90ae3650ec33af969011a9b37c4e9b2835970de
Danny Tal'or <dannytalor96@gmail.com>
2024-09-16 17:48:58 +0300

    Create UserRoleConverter.cs

---
+++ UserRoleConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SCE24_BioMedSW_Blood_Establishment_WPF
+{
+    public class UserRoleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int roleNumber)
+            {
+                return Util.GetUserRole(roleNumber);
+            }
+            return "None";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string roleTitle)
+            {
+                return Util.GetUserRole(roleTitle);
+            }
+            return -1;
+        }
+    }
+}
